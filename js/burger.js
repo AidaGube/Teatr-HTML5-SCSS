f1c1d703679be1d60cd3761186f97f432bf7f010
Aida <aidagube@gmail.com>
2023-05-30 18:06:46 +0600

    style: added styles for adaptations

--- js/burger.js
+++ js/burger.js
@@ -1,12 +1,10 @@
 const iconMenu = document.querySelectorAll('.header__burger');
 
 if (iconMenu) {
-
 	const menuHeader = document.querySelector('.header__menu');
 	iconMenu.addEventListener("click", function(e) {
 		document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
 	menuHeader.classList.toggle('_active');
 	});
-
 }
